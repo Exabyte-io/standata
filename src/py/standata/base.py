cb820a2554418730e925b61c8c5bb81a619110fd
Alexander Zech <alexzech@gmx.de>
2023-08-18 20:22:43 -0700

    docs: add docstrings + more type hints

--- src/py/standata/base.py
+++ src/py/standata/base.py
@@ -1,27 +1,45 @@
 import json
 from pathlib import Path
-from typing import List, Optional, TypedDict, Union
+from typing import Dict, List, Optional, TypedDict, Union
 
 import pandas as pd
 import yaml
 
 EntityItem = TypedDict("EntityItem", {"filename": str, "categories": List[str]})
 
-EntityConfig = TypedDict("EntityConfig", {"categories": dict, "entities": List[EntityItem]})
+EntityConfig = TypedDict("EntityConfig", {"categories": Dict[str, str], "entities": List[EntityItem]})
 
 
 class Standata:
+    """The Standata class associates the entity data files with categories and allows for tag-based queries.
+
+    Attributes:
+        entity_config_path: Path to the entity config file (categories.yml).
+        entity_dir: Path to the folder containing entity data files.
+        entities: List of entity items specifying filename and categories.
+        category_map: Dictionary mapping category types to category tags.
+        categories: List of unique categories from flattening the category_map dictionary.
+        lookup_table: Category-filename lookup table.
+
+    """
+
     def __init__(self, entity_config_path: Union[str, Path]):
-        self.entity_config_path = Path(entity_config_path).resolve()
-        self.entity_dir = self.entity_config_path.parent
+        """Initializes categories and the entity list.
+
+        Args:
+             entity_config_path: The path to the entity config file `categories.yml`.
+        """
+        self.entity_config_path: Path = Path(entity_config_path).resolve()
+        self.entity_dir: Path = self.entity_config_path.parent
 
         cfg = self.__load_config()
-        self.entities = cfg["entities"]
-        self.category_map = cfg["categories"]
-        self.categories = self.__flatten_categories()
-        self.lookup_table = self.__create_table()
+        self.entities: List[EntityItem] = cfg["entities"]
+        self.category_map: Dict[str, str] = cfg["categories"]
+        self.categories: List[str] = self.__flatten_categories()
+        self.lookup_table: pd.DataFrame = self.__create_table()
 
     def __load_config(self) -> EntityConfig:
+        """Loads entity config from file (Yaml)."""
         entity_config: EntityConfig = {"categories": {}, "entities": []}
         try:
             with open(self.entity_config_path, "r") as stream:
@@ -31,13 +49,37 @@ class Standata:
         return entity_config
 
     def __flatten_categories(self, separator: str = "/") -> List[str]:
+        """Flattens categories dictionary to list of categories.
+
+        Example::
+
+            self.categories = {"size": ["S", "M", "L"]}
+            self.__flatten_categories() # returns ["size/S", "size/M", "size/L"]
+        """
         category_groups = [list(map(lambda x: f"{key}{separator}{x}", val)) for key, val in self.category_map.items()]
         return [item for sublist in category_groups for item in sublist]
 
-    def convert_tag_to_category(self, *tags):
+    def convert_tag_to_category(self, *tags: str):
+        """Converts simple tags to '<category_type>/<tag>' format.
+
+        Args:
+            *tags: Category tags for the entity.
+
+        Note:
+            Some tags belong to several categories simultaneously, for instance 'semiconductor' is associated with
+            'electrical_conductivity' and 'type'. This function returns all occurrences of a tag as
+            '<category_type>/<tag>'.
+        """
         return [cf for cf in self.categories if any([cf.endswith(t) for t in tags])]
 
     def __create_table(self) -> pd.DataFrame:
+        """Creates lookup table for filenames and associated categories.
+
+        For the lookup table category tags are first converted to the <category_type>/<tag> format, which represent the
+        columns of the lookup table. The filenames represent the rows of the lookup table (DataFrame.index). The values
+        in the table are either 0 or 1 depending on whether a filename is associated with a certain category (1) or
+        not (0).
+        """
         df = pd.DataFrame(0, columns=self.categories, index=[entity["filename"] for entity in self.entities])
         for entity in self.entities:
             filename = entity["filename"]
@@ -46,7 +88,13 @@ class Standata:
                 df.loc[filename, category] = 1
         return df
 
-    def __get_filenames(self, *categories) -> List[Path]:
+    def __get_filenames(self, *categories: str) -> List[Path]:
+        """Returns filepaths to entities that match all given categories.
+
+        Args:
+            *categories: Categories for the entity query. Note, that `categories` should be in the same format as the
+            column names in the lookup table.
+        """
         if len(categories) == 0:
             return []
         mask = pd.Series([True] * len(self.lookup_table), index=self.lookup_table.index)
@@ -57,6 +105,11 @@ class Standata:
 
     @staticmethod
     def load_entity(filepath: Path) -> Optional[dict]:
+        """Loads entity config from file (JSON).
+
+        Args:
+            filepath: Path to entity data file (JSON).
+        """
         entity = None
         if not filepath.resolve().exists():
             print(f"Could not find entity file: {filepath.resolve()}")
@@ -68,7 +121,12 @@ class Standata:
             print(e)
         return entity
 
-    def find_entities(self, *tags) -> List[dict]:
+    def find_entities(self, *tags: str) -> List[dict]:
+        """Finds entities that match all specified category tags.
+
+        Args:
+            *tags: Category tags for the entity query.
+        """
         categories = self.convert_tag_to_category(*tags)
         filenames = list(map(Path, self.__get_filenames(*categories)))
         entities = []
