bcb75da30fd1de881970556a263830a0e7b345f9
Alexander Zech <alexzech@gmx.de>
2023-08-21 20:10:16 -0700

    chore: add build script for entities and entity map

---
+++ build_entity_map.js
@@ -0,0 +1,29 @@
+/**
+ * build_entity_map uses node API to read all entity category files from the FS
+ * at build time and writes them out to a single JSON file for
+ * downstream consumption to avoid FS calls in the browser.
+ */
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require("fs");
+const path = require("path");
+const yaml = require("js-yaml");
+
+function buildAsset({ assetPath, targetPath }) {
+    const fileContent = fs.readFileSync(assetPath, { encoding: "utf-8" });
+    const obj = yaml.load(fileContent);
+
+    obj.entities?.forEach((entity) => {
+        const entityPath = path.join(path.dirname(assetPath), entity.filename);
+        const content = fs.readFileSync(path.resolve(entityPath), { encoding: "utf-8" });
+        obj[entity.filename] = JSON.parse(content);
+    });
+
+    fs.writeFileSync(targetPath, JSON.stringify(obj) + "\n", "utf8");
+    console.log(`Written entity category map to "${assetPath}" to "${targetPath}"`);
+}
+
+buildAsset({
+    assetPath: "./materials/categories.yml",
+    targetPath: "./src/js/entities/materials.json",
+});
