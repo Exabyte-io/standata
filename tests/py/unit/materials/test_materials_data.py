78f1ea327314e321624bd7759f1035a0c6e769ff
Timur Bazhirov <timur@exabyte.io>
2023-12-26 04:01:49 -0800

    chore: fix typo

--- tests/py/unit/materials/test_materials_data.py
+++ tests/py/unit/materials/test_materials_data.py
@@ -1,7 +1,7 @@
 from mat3ra.standata.materials import materials_data
 
 
-def test_find_entity_tag():
+def test_get_material_data():
     """Assert correct information if found about a material."""
     material = materials_data["filesMapByName"]["Graphene.json"]
     assert type(material) == dict
