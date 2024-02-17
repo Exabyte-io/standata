b1fbfb9a7f1d63153657d7494b1ec85b55f0d7ce
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-02-17 12:52:28 -0800

    chore: update test

--- tests/js/material.test.ts
+++ tests/js/material.test.ts
@@ -3,9 +3,9 @@ import { expect } from "chai";
 import h_BN from "../../materials/BN-[Hexagonal_Boron_Nitride]-HEX_[P6_3%2Fmmc]_3D_[Bulk]-[mp-984].json";
 import Graphene from "../../materials/C-[Graphene]-HEX_[P6%2Fmmm]_2D_[Monolayer]-[mp-1040425].json";
 import Na4Cl4 from "../../materials/NaCl-[Sodium_Chloride]-CUB_[Fm-3m]_3D_[Bulk]-[mp-22862].json";
-import WS2 from "../../materials/WS2_001.json";
+import Si from "../../materials/Si-[Silicon]-FCC_[Fd-3m]_3D_[Bulk]-[mp-149].json";
+import WS2 from "../../materials/WS2-[Tungsten_Disulfide]-HEX_[P-6m2]_2D_[Monolayer]-[m3-7oJTkFFyBgNPRGe6M].json";
 import { MaterialStandata } from "../../src/js";
-import Si from "./fixtures/Si.json";
 
 describe("Materials Standata", () => {
     it("can search materials by tags", () => {
