ef3a5699a5a77be5ac7550deeef8ca6b407c6589
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-02-17 14:11:47 -0800

    chore: fix py test

--- tests/py/unit/materials/test_materials_data.py
+++ tests/py/unit/materials/test_materials_data.py
@@ -3,7 +3,7 @@ from mat3ra.standata.materials import materials_data
 
 def test_get_material_data():
     """Assert correct information if found about a material."""
-    material = materials_data["filesMapByName"]["Graphene.json"]
+    material = materials_data["filesMapByName"]["C-[Graphene]-HEX_[P6%2Fmmm]_2D_[Monolayer]-[mp-1040425].json"]
     assert type(material) == dict
-    assert material["name"] == "Graphene (mp-1040425)"
+    assert material["name"] == "C, Graphene, HEX (P6/mmm) 2D (Monolayer), mp-1040425"
     assert material["isNonPeriodic"] is False
