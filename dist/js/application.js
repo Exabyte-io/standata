8b83a41866667288c10ba6d5f268551def5887d3
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-12-19 12:50:43 -0800

    chore: commit dist

---
+++ dist/js/application.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault =
+    (this && this.__importDefault) ||
+    function (mod) {
+        return mod && mod.__esModule ? mod : { default: mod };
+    };
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ApplicationStandata = void 0;
+const base_1 = require("./base");
+const applications_json_1 = __importDefault(require("./runtime_data/applications.json"));
+class ApplicationStandata extends base_1.Standata {}
+exports.ApplicationStandata = ApplicationStandata;
+ApplicationStandata.runtimeData = applications_json_1.default;
---
+++ dist/js/base.d.ts
@@ -0,0 +1,35 @@
+interface EntityItem {
+    filename: string;
+    categories: string[];
+}
+interface EntityCategories {
+    [key: string]: string[];
+}
+export interface StandataConfig {
+    categories: EntityCategories;
+    entities: EntityItem[];
+}
+interface RuntimeData {
+    standataConfig: StandataConfig;
+    filesMapByName: object;
+}
+export declare class Standata {
+    static runtimeData: RuntimeData;
+    static getRuntimeDataConfigs(): any[];
+    entities: EntityItem[];
+    categories: string[];
+    protected categoryMap: EntityCategories;
+    protected lookupTable: {
+        [key: string]: Set<string>;
+    };
+    constructor(config?: StandataConfig);
+    flattenCategories(separator?: string): string[];
+    convertTagToCategory(...tags: string[]): string[];
+    protected createLookupTable(): {
+        [key: string]: Set<string>;
+    };
+    protected loadEntity(filename: string): object | undefined;
+    protected filterByCategories(...categories: string[]): string[];
+    findEntitiesByTags(...tags: string[]): object[];
+}
+export {};
