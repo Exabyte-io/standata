56c734838cd77e07c5f12c57c438e51ffeebc1f9
VsevolodX <vsevolodbiryukov@icloud.com>
2025-09-02 18:44:29 -0700

    update: set ids to be fied

--- workflows/wode/generate_workflows.js
+++ workflows/wode/generate_workflows.js
@@ -9,12 +9,33 @@ const workflowConfigs = createWorkflowConfigs(null, [], workflowData);
 
 const workflowsDir = path.resolve(__dirname, "..");
 
+const returnConfigWithFixedIds = (config) => {
+    const hash = require("crypto")
+        .createHash("md5")
+        .update(config.name)
+        .digest("hex")
+        .substring(0, 8);
+    const baseId = `${hash}-0000-0000-0000-000000000000`;
+
+    const fixIds = (obj) => {
+        if (obj._id) obj._id = baseId;
+        if (obj.flowchartId) obj.flowchartId = baseId;
+        Object.values(obj).forEach((val) => {
+            if (val && typeof val === "object") fixIds(val);
+        });
+    };
+
+    fixIds(config);
+    return config;
+};
+
 workflowConfigs.forEach((config) => {
+    const deterministicConfig = returnConfigWithFixedIds(config);
     const appName = config.subworkflows?.[0]?.application?.name || "unknown";
     const workflowName = config.name.toLowerCase().replace(/[^a-z0-9]/g, "_");
     const filename = `${appName}_${workflowName}.json`;
     const filePath = path.resolve(workflowsDir, filename);
-    fs.writeFileSync(filePath, JSON.stringify(config, null, 2), "utf8");
+    fs.writeFileSync(filePath, JSON.stringify(deterministicConfig, null, 2), "utf8");
 
     console.log(`Generated: ${filename}`);
 });
