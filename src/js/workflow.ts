052d6e6438536f7acff51db25a250d104e5a8d23
VsevolodX <vsevolodbiryukov@icloud.com>
2025-09-17 12:19:12 -0700

    update: add helper functions

--- src/js/workflow.ts
+++ src/js/workflow.ts
@@ -18,6 +18,18 @@ export class WorkflowStandata extends Standata {
     findByApplicationAndName(appName: string, displayName: string) {
         return this.findByApplication(appName).find((w: any) => w?.name === displayName);
     }
+
+    findRelaxationWorkflowByApplicationName(appName: string) {
+        return this.findEntitiesByTags("relaxation", appName)[0];
+    }
+
+    findDefault() {
+        const defaults = this.findEntitiesByTags("default");
+        if (defaults.length > 1) {
+            console.warn("More than one default workflow found!");
+        }
+        return defaults[0];
+    }
 }
 
 export class SubworkflowStandata extends Standata {
