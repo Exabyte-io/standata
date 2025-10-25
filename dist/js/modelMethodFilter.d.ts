1595a55cfcfe5cc5589905b392a28c00ebc2dfb9
VsevolodX <vsevolodbiryukov@icloud.com>
2025-10-24 17:46:29 -0700

    chore: regen dist

--- dist/js/modelMethodFilter.d.ts
+++ dist/js/modelMethodFilter.d.ts
@@ -15,3 +15,15 @@ export declare class ModelMethodFilter {
     getUniqueFilterPaths(): string[];
     getUniqueFilterRegexes(): string[];
 }
+/**
+ * Convenience function to filter methods by model
+ * This is a helper wrapper around ModelMethodFilter.getCompatibleMethods()
+ *
+ * @param methodList - Array of method configs to filter
+ * @param model - Model config to use for filtering
+ * @returns Filtered array of compatible method configs
+ */
+export declare function filterMethodsByModel({ methodList, model, }: {
+    methodList: MethodConfig[];
+    model?: ModelConfig;
+}): MethodConfig[];
