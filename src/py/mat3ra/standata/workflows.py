909b05a85378467897be391025f809a0171c1918
VsevolodX <vsevolodbiryukov@icloud.com>
2025-12-05 18:58:11 -0800

    update: filter by app

--- src/py/mat3ra/standata/workflows.py
+++ src/py/mat3ra/standata/workflows.py
@@ -1,4 +1,6 @@
-from typing import Dict
+from typing import Dict, Union
+
+from mat3ra.ade import Application
 
 from .base import Standata, StandataData
 from .data.workflows import workflows_data
@@ -9,5 +11,12 @@ class WorkflowStandata(Standata):
     data: StandataData = StandataData(data_dict)
 
     @classmethod
-    def filter_by_application(cls, application: str) -> "WorkflowStandata":
+    def filter_by_application(cls, application: Union[str, Application]) -> "WorkflowStandata":
+        if isinstance(application, Application):
+            application = application.name
         return cls.filter_by_tags(application)
+
+    @classmethod
+    def filter_by_application_config(cls, application_config: Dict) -> "WorkflowStandata":
+        application_name = application_config.get("name", "")
+        return cls.filter_by_application(application_name)
