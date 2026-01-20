a934467e77a7553280d23ccd6b0128ae839eceeb
VsevolodX <vsevolodbiryukov@icloud.com>
2026-01-19 18:56:22 -0800

    update: rename

--- assets/applications/input_files_templates/python/plot_wavefunction.pyi
+++ assets/applications/input_files_templates/python/plot_wavefunction.pyi
@@ -29,22 +29,3 @@ ax.grid(True, alpha=0.3)
 plt.tight_layout()
 plt.savefig('wf_r.png', dpi=150, bbox_inches='tight')
 plt.close()
-
-# Create potential_profile JSON for platform rendering
-wavefunction_data = {
-    "name": "wavefunction_amplitude",
-    "xAxis": {
-        "label": "Position",
-        "units": "angstrom"
-    },
-    "xDataArray": z.tolist(),
-    "yAxis": {
-        "label": "Wavefunction Amplitude",
-        "units": "a.u."
-    },
-    "yDataSeries": [psi_amplitude.tolist()]
-}
-
-# Print JSON to STDOUT (will be captured as potential_profile result)
-print(json.dumps(wavefunction_data, indent=2))
-
