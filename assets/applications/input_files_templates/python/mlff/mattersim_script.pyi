ab6741b2b933c162ea740c795806f7f9ada60e43
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2026-04-11 14:52:11 +0800

    use mat3ra-made to build ase structure from json

--- assets/applications/input_files_templates/python/mlff/mattersim_script.pyi
+++ assets/applications/input_files_templates/python/mlff/mattersim_script.pyi
@@ -14,20 +14,20 @@
 # ---------------------------------------------------------------- #
 
 import torch
-from ase.build import bulk
 from ase.units import GPa
+from mat3ra.made.tools.convert import to_ase
 from mattersim.forcefield import MatterSimCalculator
+from munch import Munch
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Running MatterSim on {device}")
 
-material = {{ MATERIAL }}
-print(f"Material from io context: {material}")
+material_json = {% raw %}{{ MATERIAL }}{% endraw %}
+material = to_ase(dict(material_json))
 
-si = bulk("Si", "diamond", a=5.43)
-si.calc = MatterSimCalculator(device=device)
-print(f"Energy (eV)                 = {si.get_potential_energy()}")
-print(f"Energy per atom (eV/atom)   = {si.get_potential_energy()/len(si)}")
-print(f"Forces of first atom (eV/A) = {si.get_forces()[0]}")
-print(f"Stress[0][0] (eV/A^3)       = {si.get_stress(voigt=False)[0][0]}")
-print(f"Stress[0][0] (GPa)          = {si.get_stress(voigt=False)[0][0] / GPa}")
+material.calc = MatterSimCalculator(device=device)
+print(f"Energy (eV)                 = {material.get_potential_energy()}")
+print(f"Energy per atom (eV/atom)   = {material.get_potential_energy()/len(material)}")
+print(f"Forces of first atom (eV/A) = {material.get_forces()[0]}")
+print(f"Stress[0][0] (eV/A^3)       = {material.get_stress(voigt=False)[0][0]}")
+print(f"Stress[0][0] (GPa)          = {material.get_stress(voigt=False)[0][0] / GPa}")
