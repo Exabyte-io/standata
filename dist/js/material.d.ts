a688c82284cdcf3c26c05a07c3163202f99efadd
VsevolodX <vsevolodbiryukov@icloud.com>
2026-05-21 20:37:36 -0700

    update: add elemental Si

--- dist/js/material.d.ts
+++ dist/js/material.d.ts
@@ -226,6 +226,9 @@ export declare class MaterialStandata extends Standata {
                     origin: boolean;
                 };
                 isNonPeriodic: boolean;
+                metadata: {
+                    element: string;
+                };
             };
             "Si-[Silicene]-HEX_[P-3m1]_2D_[Monolayer]-[2dm-5934].json": {
                 name: string;
